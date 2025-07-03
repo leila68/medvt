b634a4a46b23eb5593770fb8875528b687dd296e
Leila <leila.cheshmi@gmail.com>
2025-07-03 13:50:44 -0400

    last narval update

--- avos/datasets/path_config.py
+++ avos/datasets/path_config.py
@@ -2,18 +2,19 @@
 Dataset paths.
 """
 
-davis16_train_seqs_file = '/Users/leila/Desktop/medvt/dataset/DAVIS_2016/train_seqs.txt'
-davis16_val_seqs_file = '/Users/leila/Desktop/medvt/dataset/DAVIS_2016/val_seqs.txt'
-davis16_rgb_path = '/Users/leila/Desktop/medvt/dataset/DAVIS_2016/JPEGImages/480p'
-davis16_gt_path = '/Users/leila/Desktop/medvt/dataset/DAVIS_2016/Annotations/480p'
+davis16_train_seqs_file = '/home/liila/scratch/medvt/dataset/DAVIS_2016/train_seqs.txt'
+davis16_val_seqs_file = '/home/liila/scratch/medvt/dataset/DAVIS_2016/val_seqs.txt'
+davis16_rgb_path = '/home/liila/scratch/medvt/dataset/DAVIS_2016/JPEGImages/480p'
+davis16_gt_path = '/home/liila/scratch/medvt/dataset/DAVIS_2016/Annotations/480p'
 davis16_flow_path = '/local/riemann1/home/rezaul/datasets_vos/DAVIS-data/DAVIS17/DAVIS/davis2017-flow'
 davis16_stylized_image_path = '/local/riemann/home/msiam/Stylized_DAVIS/'
 davis16_styles = ['Lynx', 'Maruska640', 'Zuzka1', 'Zuzka2']  # 'Original',
 
 
-ytvos19_rgb_path = '/local/riemann/home/rezaul/datasets2/youtube-vos-2019/train/JPEGImages'
-ytvos19_gt_path = '/local/riemann/home/rezaul/datasets2/youtube-vos-2019/train/Annotations'
+ytvos19_rgb_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/Youtube2019/train/JPEGImages'
+ytvos19_gt_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/Youtube2019/train/Annotations'
 ytvos19_flow_path = '/local/riemann1/home/rezaul/datasets_vos/Youtube-VOS18-matnet//YouTubeVOS_2018-flow'
+
 
 moca_dataset_root = '/local/riemann1/home/rezaul/datasets_vos/MoCA_filtered2'
 moca_dataset_images_path = '/local/riemann1/home/rezaul/datasets_vos/MoCA_filtered2/JPEGImages'
@@ -26,26 +27,32 @@ youtube_objects_val_set_file = '/local/riemann1/home/rezaul/datasets_vos/Youtube
 youtube_objects_masks_dir = '/local/riemann1/home/rezaul/datasets_vos/YoutubeObjects/youtube_masks/'
 
 
-kittimots_train_json_file = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_train.json'
-kittimots_val_json_file = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_val.json'
-kittimots_val_fix_json_file = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_val_fix.json'
-kittimots_rgb_training_path = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/images/training/image_02'
-kittimots_gt_path = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/annotations/375p'
-kittimots_flow_path = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS'
-kittimots_rgb_testing_path = '/Users/leila/Desktop/medvt/dataset/KITTIMOTS/images/testing/image_02'
+#kittimots_train_seqs_file = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS/train_seqs.txt'
+#kittimots_val_seqs_file = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS/val_seqs.txt'
+#kittimots_rgb_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS/images/training/image_02'
+#kittimots_gt_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS/annotations/375p'
+
+# server path
+kittimots_train_json_file = '/home/liila/scratch/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_train.json'
+kittimots_val_json_file = '/home/liila/scratch/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_val.json'
+kittimots_rgb_training_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS//images/training/image_02'
+kittimots_gt_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/KITTIMOTS/annotations/375p'
+kittimots_flow_path = '/home/liila/scratch/medvt/dataset/KITTIMOTS/'
+kittimots_rgb_testing_path = '/home/liila/scratch/medvt/dataset/KITTIMOTS/images/testing/image_02'
 kittimots_stylized_image_path = '/local/riemann/home/msiam/Stylized_DAVIS/'
 kittimots_styles = ['Lynx', 'Maruska640', 'Zuzka1', 'Zuzka2']  # 'Original',
 
-# beluga server path
-# kittimots_train_json_file = '/home/liila/medvt/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_train.json'
-# kittimots_val_json_file = '/home/liila/medvt/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_val.json'
-# kittimots_val_fix_json_file = '/home/liila/medvt/medvt/dataset/KITTIMOTS/annotations/KITTIMOTS_MOSeg_val_fix.json'
-# kittimots_rgb_training_path = '/home/liila/medvt/medvt/dataset/KITTIMOTS//images/training/image_02'
-# kittimots_gt_path = '/home/liila/medvt/medvt/dataset/KITTIMOTS/annotations/375p'
-# kittimots_flow_path = '/home/liila/medvt/medvt/dataset/KITTIMOTS/'
-# kittimots_rgb_testing_path = '/home/liila/medvt/medvt/dataset/KITTIMOTS/images/testing/image_02'
-# kittimots_stylized_image_path = '/local/riemann/home/msiam/Stylized_DAVIS/'
-# kittimots_styles = ['Lynx', 'Maruska640', 'Zuzka1', 'Zuzka2']  # 'Original',
+
+cityscapes_train_seqs_file = '/home/liila/scratch/AD-VideoSegmentation/dataset/Cityscapes/train_seqs.txt'
+cityscapes_val_seqs_file = '/home/liila/scratch/AD-VideoSegmentation/dataset/Cityscapes/val_seqs.txt'
+cityscapes_rgb_val_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/leftImg8bit_sequence/val'
+cityscapes_gt_val_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/Cityscapes/Annotations/val'
+cityscapes_rgb_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/leftImg8bit_sequence/val'
+cityscapes_gt_path = '/home/liila/scratch/AD-VideoSegmentation/dataset/Cityscapes/Annotations/train'
+cityscapes_flow_path = '/Users/leila/Desktop/medvt/dataset'
+cityscapes_flow_path = '/Users/leila/Desktop/medvt/dataset'
+cityscapes_stylized_image_path = '/Users/leila/Desktop/medvt/Stylized_Cityscapes/'
+cityscapes_styles = ['Lynx', 'Maruska640', 'Zuzka1', 'Zuzka2']  # 'Original',
 
 bdd_train_seqs_file = '/Users/leila/Desktop/medvt/dataset/BDD/train_seqs.txt'
 bdd_val_seqs_file = '/Users/leila/Desktop/medvt/dataset/BDD/val_seqs.txt'
@@ -56,3 +63,4 @@ bdd_gt_path = '/Users/leila/Desktop/medvt/dataset/BDD/Annotations/720p'
 bdd_flow_path = '/Users/leila/Desktop/medvt/dataset'
 bdd_stylized_image_path = '/Users/leila/Desktop/medvt/Stylized_BDD/'
 bdd_styles = ['Lynx', 'Maruska640', 'Zuzka1', 'Zuzka2']  # 'Original',
+
